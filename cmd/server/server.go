467125aabfbfea45d73923d59aaab8e9206cf85f
Deivid Roger Oliveira Santos <deivid@deividroger.net>
2021-03-30 22:28:14 -0300

    Add sample project

---
+++ cmd/server/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"gitub.com/deividroger/grpc-protobuf/pb"
+	"gitub.com/deividroger/grpc-protobuf/services"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:50051")
+
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
+
+	reflection.Register(grpcServer)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Could not serve %v", err)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module gitub.com/deividroger/grpc-protobuf
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.5.0
+	google.golang.org/grpc v1.36.1
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
